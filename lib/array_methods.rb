6ff41d2d21d9427975407a574dfe2744d18e36e9
Kian Edalat <akedalat@gmail.com>
2019-01-03 23:19:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,13 +14,14 @@ def using_reverse(array)
 end
 
 def using_first(array)
+  return array.first
 
 end
 
 def using_last(array)
-
+return array.last
 end
 
 def using_size(array)
-
+return array.size
 end
